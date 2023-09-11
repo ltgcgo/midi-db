b86d948ddfbf578c982d204aa15d7622634d5514
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-09-11 12:32:21 +0000

    Sort XG.

--- sh/sortXg.sh
+++ sh/sortXg.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-deno run --allow-all utils/mapOrder.js map/xg.10.tsv bank/xg.tsv bank/plg-100sg.tsv
+# Sorter
+deno run --allow-all utils/mapOrder.js map/xg.10.tsv bank/xg.tsv bank/plg-100sg.tsv > map/xg.10.tsv.sorted
+# Mover
+cd map
+ls -1 *.sorted | while IFS= read -r file; do
+	mv -v "${file}" "${file/.sorted/}"
+done
 exit
