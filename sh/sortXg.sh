a2921335eb726be5b39c8b4f881432e6e0a244d8
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-09-11 12:39:08 +0000

    All XG plugins are considered.

--- sh/sortXg.sh
+++ sh/sortXg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Sorter
-deno run --allow-all utils/mapOrder.js map/xg.10.tsv bank/xg.tsv bank/plg-100sg.tsv > map/xg.10.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/xg.10.tsv bank/xg.tsv bank/plg-150dr.tsv bank/plg-150pf.tsv bank/plg-150vl.tsv bank/plg-100sg.tsv bank/plg-150dx.tsv bank/plg-150an.tsv > map/xg.10.tsv.sorted
 # Mover
 cd map
 ls -1 *.sorted | while IFS= read -r file; do
