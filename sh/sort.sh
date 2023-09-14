9d3c3d28a714766cf4711dd7c22bcd2d3c50f010
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-09-14 10:09:34 +0000

    Minor fixes.

--- sh/sort.sh
+++ sh/sort.sh
@@ -3,6 +3,12 @@
 deno run --allow-all utils/mapOrder.js map/xg.10.tsv bank/xg.tsv bank/plg-150dr.tsv bank/plg-150pf.tsv bank/plg-150vl.tsv bank/plg-100sg.tsv bank/plg-150dx.tsv bank/plg-150an.tsv > map/xg.10.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/xg.12.tsv bank/xg.tsv bank/plg-150dr.tsv bank/plg-150pf.tsv bank/plg-150vl.tsv bank/plg-100sg.tsv bank/plg-150dx.tsv bank/plg-150an.tsv > map/xg.12.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/xg.24.tsv bank/xg.tsv bank/plg-150dr.tsv bank/plg-150pf.tsv bank/plg-150vl.tsv bank/plg-100sg.tsv bank/plg-150dx.tsv bank/plg-150an.tsv > map/xg.24.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/gs.10.tsv bank/gs.tsv > map/gs.10.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/gs.12.tsv bank/gs.tsv > map/gs.12.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/gs.24.tsv bank/gs.tsv > map/gs.24.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/ns5r.10.tsv bank/ns5r.tsv > map/ns5r.10.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/ns5r.12.tsv bank/ns5r.tsv > map/ns5r.12.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/ns5r.24.tsv bank/ns5r.tsv > map/ns5r.24.tsv.sorted
 # Mover
 cd map
 ls -1 *.sorted | while IFS= read -r file; do
