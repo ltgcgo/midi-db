a8756a6e4f86439a8b6b414d3fbb3f8da0b8efd6
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-03-31 03:41:36 +0000

    Sort.

--- sh/sort.sh
+++ sh/sort.sh
@@ -12,6 +12,10 @@ deno run --allow-all utils/mapOrder.js map/ns5r.24.tsv bank/ns5r.tsv > map/ns5r.
 deno run --allow-all utils/mapOrder.js map/sd.10.tsv bank/sd.tsv > map/sd.10.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/sd.12.tsv bank/sd.tsv > map/sd.12.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/sd.24.tsv bank/sd.tsv > map/sd.24.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/s90es.10.tsv bank/s90es.tsv > map/s90es.10.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/s90es.12.tsv bank/s90es.tsv > map/s90es.12.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/s90es.24.tsv bank/s90es.tsv > map/s90es.24.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/kross.24.tsv bank/kross.tsv > map/kross.24.tsv.sorted
 # Mover
 cd map
 ls -1 *.sorted | while IFS= read -r file; do
