ce987f76c96bd4fcf07ec0b72cc3280f3c5c7e53
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-06-03 20:09:56 +0000

    Fixed the sort script because... It freaking erased all my progress on PLG-VL today!

--- sh/sort.sh
+++ sh/sort.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Sorter
-deno run --allow-all utils/mapOrder.js map/xg.10.tsv bank/xg.tsv bank/plg-150dr.tsv bank/plg-150pf.tsv bank/plg-150vl.tsv bank/plg-100sg.tsv bank/plg-150dx.tsv bank/plg-150an.tsv > map/xg.10.tsv.sorted
-deno run --allow-all utils/mapOrder.js map/xg.12.tsv bank/xg.tsv bank/plg-150dr.tsv bank/plg-150pf.tsv bank/plg-150vl.tsv bank/plg-100sg.tsv bank/plg-150dx.tsv bank/plg-150an.tsv > map/xg.12.tsv.sorted
-deno run --allow-all utils/mapOrder.js map/xg.24.tsv bank/xg.tsv bank/plg-150dr.tsv bank/plg-150pf.tsv bank/plg-150vl.tsv bank/plg-100sg.tsv bank/plg-150dx.tsv bank/plg-150an.tsv > map/xg.24.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/xg.10.tsv bank/xg.tsv bank/plg-dr.tsv bank/plg-pf.tsv bank/plg-vl.tsv bank/plg-sg.tsv bank/plg-dx.tsv bank/plg-an.tsv > map/xg.10.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/xg.12.tsv bank/xg.tsv bank/plg-dr.tsv bank/plg-pf.tsv bank/plg-vl.tsv bank/plg-sg.tsv bank/plg-dx.tsv bank/plg-an.tsv > map/xg.12.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/xg.24.tsv bank/xg.tsv bank/plg-dr.tsv bank/plg-pf.tsv bank/plg-vl.tsv bank/plg-sg.tsv bank/plg-dx.tsv bank/plg-an.tsv > map/xg.24.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/gs.10.tsv bank/gs.tsv > map/gs.10.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/gs.12.tsv bank/gs.tsv > map/gs.12.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/gs.24.tsv bank/gs.tsv > map/gs.24.tsv.sorted
