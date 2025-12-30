b5e011b263a177938526f7c38cd96f06baa9923c
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-12-30 15:28:11 +0000

    Added XG drums to Roland name mapping & tuning.

--- sh/sort.sh
+++ sh/sort.sh
@@ -5,7 +5,7 @@ deno run --allow-all utils/mapOrder.js map/xg.12.tsv bank/xg.tsv bank/plg-dr.tsv
 deno run --allow-all utils/mapOrder.js map/xg.24.tsv bank/xg.tsv bank/plg-dr.tsv bank/plg-pf.tsv bank/plg-vl.tsv bank/plg-sg.tsv bank/plg-dx.tsv bank/plg-an.tsv > map/xg.24.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/gs.10.tsv bank/gs.tsv > map/gs.10.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/gs.12.tsv bank/gs.tsv > map/gs.12.tsv.sorted
-deno run --allow-all utils/mapOrder.js map/gsCap.12.tsv bank/xg.tsv bank/gs.tsv > map/gsCap.12.tsv.sorted
+deno run --allow-all utils/mapOrder.js map/gsCap.12.tsv bank/gs.tsv bank/xg.tsv > map/gsCap.12.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/gs.24.tsv bank/gs.tsv > map/gs.24.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/ns5r.10.tsv bank/ns5r.tsv > map/ns5r.10.tsv.sorted
 deno run --allow-all utils/mapOrder.js map/ns5r.12.tsv bank/ns5r.tsv > map/ns5r.12.tsv.sorted
